22799b4df4acf7601469774792df04d5d21a176e
Marcos López <marcoslopez1895@gmail.com>
2024-03-20 22:20:19 -0400

    comenzando taxes

---
+++ app/Models/Tax.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Tax extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'fixed',
+        'percentage',
+        'name',
+    ];
+
+    protected function casts():array
+    {
+        return [
+            'fixed' => 'numeric',
+            'percentage' => 'numeric',
+        ];
+    }
+}
--- routes/auth.php
+++ routes/auth.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\Currency\CurrencyController;
+use App\Http\Controllers\Admin\Tax\TaxController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
@@ -60,4 +61,5 @@ Route::middleware('auth')->group(function () {
                 ->name('logout');
 
     Route::resource('currencies',CurrencyController::class);
+    Route::resource('taxes',TaxController::class);
 });
